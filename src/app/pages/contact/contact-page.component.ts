e2b29949d181a13bb370b5115f7b845e6df5d32c
Jose Antonio <jbregar.dev@gmail.com>
2025-01-23 08:58:14 +0100

    First commit before adding  all about angular SSR to see after what's things add Angular when we use it's SSR.

---
+++ src/app/pages/contact/contact-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'pokemon-contact',
+  imports: [],
+  templateUrl: './contact-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class ContactPageComponent {}
